f1ee44c9f989af57ee544c80bcdb9263e1cf8bae
SIVA3161 <sivaguru3161@gmail.com>
2022-04-01 19:45:23 +0530

    content page details added & videos fragment hided

---
+++ app/src/main/java/com/example/enlearn/contents/ContentActivity.kt
@@ -0,0 +1,19 @@
+package com.example.enlearn.contents
+
+import android.app.Activity
+import android.os.Bundle
+import com.example.enlearn.R
+
+/**
+ * Created by Siva G Gurusamy on 29/Mar/2022
+ * email : siva@paxel.co
+ */
+class ContentActivity : Activity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_contents)
+    }
+
+}
--- app/src/main/java/com/example/enlearn/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/enlearn/ui/home/HomeFragment.kt
@@ -1,5 +1,6 @@
 package com.example.enlearn.ui.home
 
+import android.R
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -8,8 +9,9 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import com.example.enlearn.R
 import com.example.enlearn.databinding.FragmentHomeBinding
+import kotlinx.android.synthetic.main.fragment_home.*
+
 
 class HomeFragment : Fragment() {
 
@@ -24,7 +26,7 @@ class HomeFragment : Fragment() {
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         homeViewModel =
             ViewModelProvider(this).get(HomeViewModel::class.java)
 
